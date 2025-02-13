931aac9db19ff5d68a4678a2e1928cc78cde4b03
Israel Falcão <israel.jesus@ba.estudante.senai.br>
2025-02-13 00:34:26 +0000

    novo commit

--- poo/Main.java
+++ poo/Main.java
@@ -1,13 +1,35 @@
 package poo;
 import java.util.Scanner;
 
+import poo.models.Pessoa;
+
+// CTRL + ; pode ser utilizado para comentar linhas 
+
 public class Main {
     public static void main(String[] args) {
 
             Scanner scanner = new Scanner(System.in);
             
-            System.out.println("Hello world");
-  
+            int idade;
+            String nome;
+            String email;
+
+            Pessoa pessoa = new Pessoa("", "", 0);
+            
+            System.out.println("Idade");
+            idade = scanner.nextInt();
+            System.out.println("E-mail");
+            email = scanner.nextLine();
+            System.out.println("Nome");
+            nome = scanner.nextLine();
+
+            pessoa.setIdade(idade);
+            pessoa.setEmail(email);
+            pessoa.setNome(nome);
+            
+            System.out.println("Nome: "+ pessoa.getNome());
+            System.out.println("E-mail: "+ pessoa.getEmail());
+            System.out.println("Idade: "+ pessoa.getIdade());
    
    
    
